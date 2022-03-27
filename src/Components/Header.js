b6b4589e00e887d88df8a8d80a4ea87ca48fc34d
brunorcx <brunorcx@hotmail.com>
2022-03-27 15:09:16 -0400

    fixed merge conflits and scroll function

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,37 +3,21 @@ import "../styles/header.css";
 const Header = (props) => {
   return (
     <header className="header container">
-      <img
-        src="/images/logo.png"
-        alt="logo da shey pipocas gourmet"
-        className="header__logo"
-      />
+      <img src="/images/logo.png" alt="logo da shey pipocas gourmet" className="header__logo" />
 
       <nav className="menu">
         <ul className="menu-list">
-          <li
-            className="menu-list__item"
-            onClick={() => props.setNavbarClick("Sobre")}
-          >
+          <li className="menu-list__item" onClick={() => props.setNavbarClick("Home")}>
             <a className="menu-list__link">Home</a>
           </li>
-          <li
-            className="menu-list__item"
-            onClick={() => props.setNavbarClick("Planos")}
-          >
+          <li className="menu-list__item" onClick={() => props.setNavbarClick("Sobre")}>
             <a className="menu-list__link">Sobre</a>
           </li>
-          <li
-            className="menu-list__item"
-            onClick={() => props.setNavbarClick("Modelos")}
-          >
+          <li className="menu-list__item" onClick={() => props.setNavbarClick("Fotos")}>
             <a className="menu-list__link">Fotos</a>
           </li>
-          <li
-            className="menu-list__item"
-            onClick={() => props.setNavbarClick("Contatos")}
-          >
-            <a className="menu-list__link">Contato</a>
+          <li className="menu-list__item" onClick={() => props.setNavbarClick("Contatos")}>
+            <a className="menu-list__link">Contatos</a>
           </li>
         </ul>
       </nav>
