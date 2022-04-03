36671c3dbd533d9daf630b99e7c35125db1810a6
brunorcx <brunorcx@hotmail.com>
2022-04-03 12:23:09 -0300

    Pictures grid (#14)
    * fotos section with grid and new pictures
    
    * removed border-box from header

--- src/App.js
+++ src/App.js
@@ -57,11 +57,13 @@ const App = () => {
           }
         }}
       >
-        <img
+        {/* <img
           src="/images/home-popcorn.jpg"
           alt="logo da shey pipocas gourmet"
           title="Credit: Photo by Yulia Khlebnikova at https://unsplash.com/photos/iuwMdNq0-s4?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink"
-        />
+        /> */}
+        <img src="/images/popcorn-nutella.jpg" alt="logo da shey pipocas gourmet" />
+
         <h1>Pipocas Gourmet para toda família</h1>
       </div>
       <div
@@ -84,7 +86,14 @@ const App = () => {
         }}
       >
         <h1>Fotos</h1>
-        <img src="/images/logo-shey.jpeg" alt="logo da shey pipocas gourmet" />
+        <div className="images">
+          <img src="/images/popcorn-nutella.jpg" alt="logo da shey pipocas gourmet" />
+          <img src="/images/logo-popcorn.jpg" alt="logo da shey pipocas gourmet" />
+          <img src="/images/bags.png" alt="logo da shey pipocas gourmet" />
+          <img src="/images/bags-2.png" alt="logo da shey pipocas gourmet" />
+          <img src="/images/bags-funny.png" alt="logo da shey pipocas gourmet" />
+          <img src="/images/bags-small.png" alt="logo da shey pipocas gourmet" />
+        </div>
       </div>
       <div
         className="Contatos"
