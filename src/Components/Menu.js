3592b6315f6f730fea4fccea41baa9c81f84fabf
Gabriel Machado <gramchado12@hotmail.com>
2022-04-03 11:22:38 -0400

    Just to merge

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -4,43 +4,43 @@ const Menu = ({ setNavbarClick }) => {
   return (
     <div className="menu">
       <h1 className="menu__title">Menu Pipocas Gourmet</h1>
-      <ul className="menu__flavors">
-        <li className="menu__flavors-item">
-            <h2>Ninho</h2>
-            <h2>50g</h2>
-            <h2>100g</h2>
-        </li>
-        <li className="menu__flavors-item">
-            <p>Ninho</p>
-            <p>R$9,20</p>
-            <p>R$18,40</p>
-        </li>
-        <li className="menu__flavors-item">
-            <p>Ninho</p>
-            <p>R$9,20</p>
-            <p>R$18,40</p>
-        </li>
-        <li className="menu__flavors-item">
-            <p>Ninho</p>
-            <p>R$9,20</p>
-            <p>R$18,40</p>
-        </li>
-        <li className="menu__flavors-item">
-            <p>Ninho</p>
-            <p>R$9,20</p>
-            <p>R$18,40</p>
-        </li>
-        <li className="menu__flavors-item">
-            <p>Ninho</p>
-            <p>R$9,20</p>
-            <p>R$18,40</p>
-        </li>
-        <li className="menu__flavors-item">
-            <p>Ninho</p>
-            <p>R$9,20</p>
-            <p>R$18,40</p>
-        </li>
-      </ul>
+      <table className="menu__flavors">
+        <tr className="menu__flavors-item">
+            <th>Sabor</th>
+            <th>50g</th>
+            <th>100g</th>
+        </tr>
+        <tr className="menu__flavors-item">
+            <td>Ninho</td>
+            <td>R$9,20</td>
+            <td>R$18,40</td>
+        </tr>
+        <tr className="menu__flavors-item">
+            <td>Paçoca</td>
+            <td>R$9,20</td>
+            <td>R$18,40</td>
+        </tr>
+        <tr className="menu__flavors-item">
+            <td>Nutela</td>
+            <td>R$9,20</td>
+            <td>R$18,40</td>
+        </tr>
+        <tr className="menu__flavors-item">
+            <td>Ovomaltine</td>
+            <td>R$9,20</td>
+            <td>R$18,40</td>
+        </tr>
+        <tr className="menu__flavors-item">
+            <td>Maracujá</td>
+            <td>R$9,20</td>
+            <td>R$18,40</td>
+        </tr>
+        <tr className="menu__flavors-item">
+            <td>Oreo</td>
+            <td>R$9,20</td>
+            <td>R$18,40</td>
+        </tr>
+      </table>
     </div>
   );
 };
