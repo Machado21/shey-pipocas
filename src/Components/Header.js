a9ff6dbc39490cd612294275c7095067f08b088d
Bruno RC <brunorcx@hotmail.com>
2022-04-03 15:05:24 -0400

    menu mobile translating

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -16,41 +16,40 @@ const Header = ({ setNavbarClick }) => {
 
   return (
     <header className="container header ">
-      <img
-        src="/images/logo.png"
-        alt="logo da shey pipocas gourmet"
-        className="header__logo"
-      />
-      <button
-        className="button__mobile"
-        onClick={() => setToggleMenu(!toggleMenu)}
-      >
+      <img src="/images/logo.png" alt="logo da shey pipocas gourmet" className="header__logo" />
+      <button className="button__mobile" onClick={() => setToggleMenu(!toggleMenu)}>
         <span className="hamburguer"></span>
       </button>
       <nav className="menu">
         <ul className="menu-list">
           <li
             className="menu-list__item"
-            onClick={() => setNavbarClick("Home")}
+            onClick={() => {
+              setNavbarClick("Home");
+              setToggleMenu(!toggleMenu);
+            }}
           >
             <a className="menu-list__link">Home</a>
           </li>
           <li
             className="menu-list__item"
-            onClick={() => setNavbarClick("Menu")}
+            onClick={() => {
+              setNavbarClick("Menu");
+              setToggleMenu(!toggleMenu);
+            }}
           >
             <a className="menu-list__link">Menu</a>
           </li>
           <li
             className="menu-list__item"
-            onClick={() => setNavbarClick("Fotos")}
+            onClick={() => {
+              setNavbarClick("Fotos");
+              setToggleMenu(!toggleMenu);
+            }}
           >
             <a className="menu-list__link">Fotos</a>
           </li>
-          <li
-            className="menu-list__item"
-            onClick={() => setNavbarClick("Contatos")}
-          >
+          <li className="menu-list__item" onClick={() => setNavbarClick("Contatos")}>
             <a className="menu-list__link">Contatos</a>
           </li>
         </ul>
