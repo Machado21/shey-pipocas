72b563ff18bc4764659f53b78b01ae0366b48b39
Gabriel Machado <gramchado12@hotmail.com>
2022-04-03 13:10:43 -0400

    Mobile ajuste on the menu

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -5,41 +5,45 @@ const Menu = ({ setNavbarClick }) => {
     <div className="menu">
       <h1 className="menu__title">Menu Pipocas Gourmet</h1>
       <table className="menu__flavors">
-        <tr className="menu__flavors-item">
+        <thead>
+          <tr className="table__head">
             <th>Sabor</th>
             <th>50g</th>
             <th>100g</th>
-        </tr>
-        <tr className="menu__flavors-item">
+          </tr>
+        </thead>
+        <tbody>
+          <tr className="menu__flavors-item">
             <td>Ninho</td>
             <td>R$9,20</td>
             <td>R$18,40</td>
-        </tr>
-        <tr className="menu__flavors-item">
+          </tr>
+          <tr className="menu__flavors-item">
             <td>Paçoca</td>
             <td>R$9,20</td>
             <td>R$18,40</td>
-        </tr>
-        <tr className="menu__flavors-item">
+          </tr>
+          <tr className="menu__flavors-item">
             <td>Nutela</td>
             <td>R$9,20</td>
             <td>R$18,40</td>
-        </tr>
-        <tr className="menu__flavors-item">
+          </tr>
+          <tr className="menu__flavors-item">
             <td>Ovomaltine</td>
             <td>R$9,20</td>
             <td>R$18,40</td>
-        </tr>
-        <tr className="menu__flavors-item">
+          </tr>
+          <tr className="menu__flavors-item">
             <td>Maracujá</td>
             <td>R$9,20</td>
             <td>R$18,40</td>
-        </tr>
-        <tr className="menu__flavors-item">
+          </tr>
+          <tr className="menu__flavors-item">
             <td>Oreo</td>
             <td>R$9,20</td>
             <td>R$18,40</td>
-        </tr>
+          </tr>
+        </tbody>
       </table>
     </div>
   );
