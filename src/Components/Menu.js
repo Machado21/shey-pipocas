96c59353bf2981c769490f270ca64068b85cea4c
Gabriel Machado <gramchado12@hotmail.com>
2022-04-24 14:56:28 -0400

    feat: Flavors new layout

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -4,88 +4,41 @@ const Menu = ({ setNavbarClick }) => {
   return (
     <div className="menu-table">
       <h1 className="menu__title">Menu Pipocas Gourmet</h1>
+      {/* Colocar sabores em uma linha 
+      fazer uma tabela apenas com peso e valor */}
+      <h2>Sabores</h2>
+      <div className="div__flavors">
+        <div>Ninho</div>
+        <div>Paçoca</div>
+        <div>Nutella</div>
+        <div>Ovomaltine</div>
+        <div>Maracujá</div>
+        <div>Oreo</div>
+      </div>
+      <h3>Preços</h3>
       <table className="menu__flavors">
-        <thead>
-          <tr className="table__head">
-            <th>Sabor</th>
-            <th>30g</th>
-            <th>40g</th>
-            <th>50g</th>
-            <th>60g</th>
-            <th>70g</th>
-            <th>80g</th>
-            <th>90g</th>
-            <th>100g</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr className="menu__flavors-item">
-            <td>Ninho</td>
-            <td>R$5,52</td>
-            <td>R$7,36</td>
-            <td>R$9,20</td>
-            <td>R$11,04</td>
-            <td>R$12,88</td>
-            <td>R$14,72</td>
-            <td>R$16,56</td>
-            <td>R$18,40</td>
-          </tr>
-          <tr className="menu__flavors-item">
-            <td>Paçoca</td>
-            <td>R$5,52</td>
-            <td>R$7,36</td>
-            <td>R$9,20</td>
-            <td>R$11,04</td>
-            <td>R$12,88</td>
-            <td>R$14,72</td>
-            <td>R$16,56</td>
-            <td>R$18,40</td>
-          </tr>
-          <tr className="menu__flavors-item">
-            <td>Nutela</td>
-            <td>R$5,52</td>
-            <td>R$7,36</td>
-            <td>R$9,20</td>
-            <td>R$11,04</td>
-            <td>R$12,88</td>
-            <td>R$14,72</td>
-            <td>R$16,56</td>
-            <td>R$18,40</td>
-          </tr>
-          <tr className="menu__flavors-item">
-            <td>Ovomaltine</td>
-            <td>R$5,52</td>
-            <td>R$7,36</td>
-            <td>R$9,20</td>
-            <td>R$11,04</td>
-            <td>R$12,88</td>
-            <td>R$14,72</td>
-            <td>R$16,56</td>
-            <td>R$18,40</td>
-          </tr>
-          <tr className="menu__flavors-item">
-            <td>Maracujá</td>
-            <td>R$5,52</td>
-            <td>R$7,36</td>
-            <td>R$9,20</td>
-            <td>R$11,04</td>
-            <td>R$12,88</td>
-            <td>R$14,72</td>
-            <td>R$16,56</td>
-            <td>R$18,40</td>
-          </tr>
-          <tr className="menu__flavors-item">
-            <td>Oreo</td>
-            <td>R$5,52</td>
-            <td>R$7,36</td>
-            <td>R$9,20</td>
-            <td>R$11,04</td>
-            <td>R$12,88</td>
-            <td>R$14,72</td>
-            <td>R$16,56</td>
-            <td>R$18,40</td>
-          </tr>
-        </tbody>
+        <th className="weight__table">
+          <tr className="title__table">Peso</tr>
+          <tr>30g</tr>
+          <td>40g</td>
+          <tr>50g</tr>
+          <tr>60g</tr>
+          <tr>70g</tr>
+          <tr>80g</tr>
+          <tr>90g</tr>
+          <tr>100g</tr>
+        </th>
+        <th className="value__table">
+          <tr className="title__table">Valor</tr>
+          <tr>R$5,52</tr>
+          <tr>R$7,36</tr>
+          <tr>R$9,20</tr>
+          <tr>R$11,04</tr>
+          <tr>R$12,88</tr>
+          <tr>R$14,72</tr>
+          <tr>R$16,56</tr>
+          <tr>R$18,40</tr>
+        </th>
       </table>
     </div>
   );
